c1805b31569193bed2cf13dc63ecb0757057b1b5
Nathaniel Blackburn <support@nblackburn.uk>
2016-08-23 01:01:12 +0100

    Cleanup tests

--- test/match.js
+++ test/match.js
@@ -3,14 +3,14 @@ const match = require('../source/index');
 
 test('get required parameter', t => {
 
-    const matches = match('/api/:version?/users/:id', '/api/v1/users/1');
+    const {version, id} = match('/api/:version?/users/:id', '/api/v1/users/1');
 
-    t.true(matches.id === '1');
+    t.true(id === '1');
 });
 
 test('get optional parameter', t => {
 
-    const matches = match('/api/:version?/users/:id', '/api/v1/users/1');
+    const {version, id} = match('/api/:version?/users/:id', '/api/v1/users/1');
 
-    t.true(matches.version === 'v1');
+    t.true(version === 'v1');
 });

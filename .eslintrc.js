447b4f44268d8f548bb0dd7059ac2190cc778c6b
Nathaniel Blackburn <support@nblackburn.uk>
2019-06-05 23:10:11 +0100

    Added prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+        browser: true
+    },
+    
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended'
+    ],
+    
+    plugins: [
+        'prettier'
+    ],
+    
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+    },
+
+    rules: {
+        'prettier/prettier': 'error'
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    semi: true,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5'
+};
